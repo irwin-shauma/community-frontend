7008b10ef6c2c73b0dae41bf34c9d060be339636
rizqi211199 <rizqialfiyati211199@gmail.com>
2022-07-26 16:41:33 +0700

    add event page

---
+++ src/app/pages/eventmember/eventdetail/event-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-eventdetail',
+    templateUrl: './event-detail.component.html',
+    styleUrls: ['./../eventmember.styles.css']
+})
+export class EventDetailCompoenent{}
