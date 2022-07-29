51f9c4bda7766254280093b1250652b3512e356f
irwin-shauma <irwin.shauma.rizky@gmail.com>
2022-07-29 11:10:43 +0700

    adding new dto

---
+++ src/app/dto/premium-payment-history/premium-payment-history-data.ts
@@ -0,0 +1,9 @@
+export interface PremiumPaymentHistoryData{
+    id? : string,
+    premiumPaymentHistoryCode? : string
+    userId? : string
+    premiumTypeId? : string
+    trxNo? : string
+    isActive? : boolean
+    version? : number
+}
---
+++ src/app/dto/premium-type/premium-type-data.ts
@@ -0,0 +1,8 @@
+export interface PremiumTypeData{
+    id? : string
+    premiumTypeCode? : string
+    price? : number
+    duration? : number
+    isActive? : boolean
+    version? : number
+}
