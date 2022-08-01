42670f1ba5d8157890a6b53296938ea45310aa9f
irwin-shauma <irwin.shauma.rizky@gmail.com>
2022-08-01 15:29:43 +0700

    fixing article header and article detail

--- src/app/pages/articlemember/articleheader/article-header.component.ts
+++ src/app/pages/articlemember/articleheader/article-header.component.ts
@@ -37,9 +37,6 @@ export class ArticleHeaderComponent implements OnInit {
     this.initData(this.startPage, this.maxPage+5)
     this.maxPage += this.maxPage;
   }
-  onExpandText(evt:any): void{
-    this.router.navigateByUrl('home');
-  }
 
   initData(startPage: number, maxPage : number): void {
     this.articleHeaderService.showAllArticle(startPage,maxPage).subscribe((result) => {
