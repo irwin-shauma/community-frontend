2d694c87aceb888833e796b97d68dfa5ee456be1
irwin-shauma <irwin.shauma.rizky@gmail.com>
2022-08-04 06:36:38 +0700

    Penambahan pagination di tabel article

--- src/app/pages/article/listarticle/list-article.component.ts
+++ src/app/pages/article/listarticle/list-article.component.ts
@@ -9,6 +9,9 @@ import { ArticleHeaderService } from 'src/app/service/article.service';
 import { LoginService } from 'src/app/service/login.service';
 import { UserService } from 'src/app/service/user.service';
 
+
+import { LazyLoadEvent } from 'primeng/api';
+
 @Component({
   selector: 'app-list-article',
   templateUrl: './list-article.component.html',
@@ -18,6 +21,14 @@ export class ArticleHeaderListComponent implements OnInit{
   articleHeaders: ArticleHeaderFindAll = {} as ArticleHeaderFindAll;
   articleHeaderData!: ArticleHeaderData[];
   deleteSubscription?: Subscription;
+
+
+  startPage: number = 0
+  maxPage: number = 5
+  totalData: number = 0
+  loading: boolean = true
+  articleSubscription? : Subscription
+
 
   constructor(
     private confirmationService: ConfirmationService,
@@ -31,8 +42,47 @@ export class ArticleHeaderListComponent implements OnInit{
   ngOnInit(): void{
     this.articleHeaders.data = [];
     this.initData();
-    
   }
+
+  loadData(event: LazyLoadEvent) {
+    this.getData(event.first, event.rows)
+}
+  getData(startPage: number = this.startPage, maxPage: number = this.maxPage): void {
+    this.loading = true;
+    this.startPage = startPage
+    this.maxPage = maxPage
+
+    this.articleSubscription = this.articleHeaderService.showAllArticle(startPage, maxPage,).subscribe(
+        result => {
+            const resultData: any = result
+            this.articleHeaderData = resultData.data
+            this.loading = false
+            this.totalData = resultData.count
+            console.log(result)
+        },
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   initData(): void{
     this.articleHeaderService.showAllArticle(0,5).subscribe((result)=>{
@@ -40,12 +90,10 @@ export class ArticleHeaderListComponent implements OnInit{
       this.articleHeaderData = result.data!;
     });
 
-    console.log(this.loginService.getData()?.data?.id);
     
     
 
     this.userService.findById( String(this.loginService.getData()?.data?.id)).subscribe((result) => {
-      console.log(result)
     })
   }
 
